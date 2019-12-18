3818e1579929cd89d9f3c37cbe196ff851affdd9
Aryan Khadiri <aryankhadiri@gmail.com>
2019-12-17 20:45:51 -0800

    Initial Commit

---
+++ iTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTip
+//
+//  Created by Aryan Khadiri on 12/17/19.
+//  Copyright © 2019 Aryan Khadiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
